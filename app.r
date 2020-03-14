4ef7c3a0ae7862761ad843acadcfaabe8d39f653
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-03-13 23:02:18 -0400

    Update app.r

--- app.r
+++ app.r
@@ -1,3 +1,4 @@
+#place the app.r and gapMinder_shiny.R in same directory.
 source("gapMinder_shiny.R")
 #alternate run
 shiny::shinyApp(ui= ui, server= server, options= list(height= 500, width= 500, scipen= 9999))
