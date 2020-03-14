e056ebb6af32b2bdde9e587bb4f52af0a38e93dd
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-03-13 23:01:24 -0400

    Update app.r

--- app.r
+++ app.r
@@ -1,4 +1,3 @@
 source("gapMinder_shiny.R")
-
+#alternate run
 shiny::shinyApp(ui= ui, server= server, options= list(height= 500, width= 500, scipen= 9999))
-shiny::runGitHub("gapminder_shiny_app_R", "opendatasurgeon")
