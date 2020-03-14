2e30f8d335c97a099e2285b47963bb21c1cb080a
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-03-13 22:28:39 -0400

    Add files via upload

--- gapMinder_shiny.R
+++ gapMinder_shiny.R
@@ -1,16 +1,16 @@
 #function that checks, & install the packages
 install_packs <- function(x){
   for( i in x ){
-    #  require returns TRUE invisibly if it was able to load package
+    #require returns TRUE invisibly if it was able to load package
     if( ! require( i , character.only = TRUE ) ){
       #  If package was not able to be loaded then re-install
       install.packages( i , dependencies = TRUE )
-      #  Load package after installing
+      #Load package after installing
       require( i , character.only = TRUE )
     }
   }
 }
-#  Then try/install packages...
+#Then try/install packages...
 install_packs(c("ggplot2" , "shiny" , "shinyWidgets", "shinythemes", "gapminder") )
 
 # library(shiny)
@@ -22,16 +22,21 @@ install_packs(c("ggplot2" , "shiny" , "shinyWidgets", "shinythemes", "gapminder"
 #ui module
 ui <- fluidPage(
   
-  theme = shinytheme("flatly"), 
+  #shiny app universal theme
+  theme = shinythemes::shinytheme("flatly"), 
   
-  titlePanel(div(HTML("<em>Life Expectancy vs. GDP per Continent & Year</em>"))),
+  #shiny app title
+  shiny::titlePanel(div(HTML("<em>Life Expectancy vs. GDP per Continent & Year</em>"))),
   
-  sidebarLayout(
+  #layout
+  shiny::sidebarLayout(
     position= c("left", "right"), fluid= TRUE,
     
-    sidebarPanel(
-
-      pickerInput(
+    #left & right panels
+    shiny::sidebarPanel(
+      
+      #offers a dropdown menu to select countries
+      shinyWidgets::pickerInput(
         inputId = "var_country", 
         label = "Select a country:", 
         choices = sample(levels(gapminder$country), 142), 
@@ -42,13 +47,13 @@ ui <- fluidPage(
         ), 
         multiple= FALSE
       ),
-      
-      prettyCheckbox(
-        inputId = "var_continent", label= strong("Color Each Continent?"), icon= icon("check"), value= TRUE,
+      #offers an animated selectable box to enable/disable colors
+      shinyWidgets::prettyCheckbox(
+        inputId = "var_continent", label= strong("Color each continent?"), icon= icon("check"), value= TRUE,
         status = "default", shape = "curve", animation = "pulse"
       ),
-      
-      sliderInput("var_year", "Year:",
+      # year time scale
+      shiny::sliderInput("var_year", "Year:",
                   min= min(gapminder$year),
                   max= max(gapminder$year),
                   value= min(gapminder$year), 
@@ -57,20 +62,23 @@ ui <- fluidPage(
                   animate= animationOptions(interval= 1500, loop= TRUE)
       )
     ),
-    
-    mainPanel(plotOutput("gapPlot"))
+    #panel where the graph will be shown
+    shiny::mainPanel(plotOutput("gapPlot"))
   )
   
 )
 #server module
 server <- function(input, output) {
   
-  output$gapPlot <- renderPlot({
+  output$gapPlot <- shiny::renderPlot({
     
+    #allows year transitions back and forth
     current_year <- gapminder[gapminder$year == input$var_year, ]
+    #allows to glue a label on a bubble
     current_country <- which(current_year$country == input$var_country)
     pop_range <- range(gapminder$pop)
     
+    #ggplot2 plot layer
     gp <-  ggplot2::ggplot(data= current_year, aes(x= gdpPercap, y= lifeExp, size = pop)) +
             ggplot2::geom_point(alpha= 0.25) + ylim(20, 90) + 
             ggplot2::geom_text(data= current_year, aes(x= gdpPercap[current_country], y= lifeExp[current_country]), 
@@ -83,6 +91,7 @@ server <- function(input, output) {
             ggplot2::theme(plot.caption = element_text(face= "italic"), 
                            plot.caption.position =  "plot")
     
+    #colour box option
     if (!input$var_continent) { 
       print(gp) 
     }
@@ -95,4 +104,4 @@ server <- function(input, output) {
   
 }
 #Build the shiny module
-shinyApp(ui= ui, server= server, options= list(height= 500, width= 500, scipen= 9999))
+shiny::shinyApp(ui= ui, server= server, options= list(height= 500, width= 500, scipen= 9999))
